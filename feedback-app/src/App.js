3549dcd7c68505791a097c5d49858d707d127e73
jissmon jose <jissmon.jose@naicoits.com>
2022-11-07 23:40:22 +0530

    Prop types

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,33 +1,14 @@
 import React, { useState } from 'react'
+import Header from './components/Header'
 
 export default function App() {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'comment 1'},
-        {id: 2, text: 'comment 2'},
-        {id: 3, text: 'comment 3'},
-        {id: 4, text: 'comment 4'}
-    ]
-    const showComments = true
   return (
-    <div className='container'>
-        <h1>
-            {title}
-        </h1>
-        <p>{body}</p>
-        {showComments && (
-            <div className='comments'>
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    {
-                        comments.map((comment, index) => (
-                            <li key={index}>{comment.text}</li>
-                        ))
-                    }
-                </ul>
-            </div>
-        )}
-    </div>
+    <>
+        <Header text="Hello World" />
+        <div className='container'> 
+            <h2>Hello Jissmon</h2>
+        </div>
+    </>
+    
   )
 }
